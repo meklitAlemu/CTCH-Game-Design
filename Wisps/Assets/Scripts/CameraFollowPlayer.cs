1dc7997ef9de1f2eb9bc02fd5b40f5d00868e8dc
Mackenzie Kot <mlk946@uregina.ca>
2024-03-16 14:58:16 -0600

    import assets, camera follows player

---
+++ Wisps/Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.rotation = Quaternion.Euler(25, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(0, 5, -10);
+    }
+}
