3c7362e6becab61cc624339ffdd01005249e2efb
meklitAlemu <mekliteshetu2@gmail.com>
2024-03-22 15:34:00 -0600

    Adjusted main menu

--- Astroscout/Assets/Scripts/MainMenu.cs
+++ Astroscout/Assets/Scripts/MainMenu.cs
@@ -6,28 +6,45 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public GUISkin skin;
-    void OnGUI(){
-        GUI.skin = skin;
-        GUI.Label (new Rect(10, 10, 400, 75), "Home");
+    // public GUISkin skin;
+    // void OnGUI(){
+    //     GUI.skin = skin;
+    //     GUI.Label (new Rect(200, 150, 700, 120), "ASTROSCOUT");
+    //     if (PlayerPrefs.GetInt("LevelCompleted") > 0){
+    //         // continue game
+    //         Boolean continueButton = GUI.Button(new Rect(10, 100, 100, 45), "Continue");
+    //         if(continueButton){
+    //             SceneManager.LoadScene(PlayerPrefs.GetInt("LevelCompleted"));   
+    //         }
+    //     }
+    //     Boolean playButton = GUI.Button(new Rect(400, 300, 100, 45), "New Game");
+    //     // start at Level 1 and load scene
+    //     if(playButton){
+    //         SceneManager.LoadScene(0);
+    //         PlayerPrefs.SetInt("LevelCompleted", 0);
+    //     }
+    //     // quit game
+    //     Boolean quitButton = GUI.Button(new Rect(400, 355, 100, 45), "Quit");
+    //     if(quitButton){
+    //         // Application.Quit();
+    //     }
+    // }
+    public void NewAdventure(){
+        SceneManager.LoadScene(0);
+        PlayerPrefs.SetInt("LevelCompleted", 0);
+        
+    }
+
+    public void Continue(){
         if (PlayerPrefs.GetInt("LevelCompleted") > 0){
             // continue game
-            Boolean continueButton = GUI.Button(new Rect(10, 100, 100, 45), "Continue");
-            if(continueButton){
-                SceneManager.LoadScene(PlayerPrefs.GetInt("LevelCompleted"));   
-            }
+            SceneManager.LoadScene(PlayerPrefs.GetInt("LevelCompleted"));   
+            
         }
-        Boolean playButton = GUI.Button(new Rect(10, 155, 100, 45), "New Game");
-        // start at Level 1 and load scene
-        if(playButton){
-            SceneManager.LoadScene(0);
-            PlayerPrefs.SetInt("LevelCompleted", 0);
-        }
-        // quit game
-        Boolean quitButton = GUI.Button(new Rect(10, 210, 100, 45), "Quit");
-        if(quitButton){
-            // Application.Quit();
-        }
+    }
+
+    public void Quit(){
+        Application.Quit();
     }
     // // Start is called before the first frame update
     // void Start()
